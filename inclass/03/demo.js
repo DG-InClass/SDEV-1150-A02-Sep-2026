b8a90834c800d0501ac05b6100a07341baa24e76
Dan Gilleland <dgilleland@nait.ca>
2026-09-15 14:12:10 -0600

    Explore primitive values

--- inclass/03/demo.js
+++ inclass/03/demo.js
@@ -1,3 +1,14 @@
 console.log('Lesson 03 demo.js has loaded');
 console.log('=============================');
 console.log();
+
+console.log('Primitive values');
+console.log('----------------');
+
+let workshopTitle = 'Intro to Digital Photography';
+let seatsAvailable = 16;
+let registrationOpen = true; // boolean     true | false
+
+console.log(`The value "${workshopTitle}" is a ${typeof workshopTitle}.`);
+console.log(`The value ${seatsAvailable} is a ${typeof seatsAvailable}.`);
+console.log(`The value ${registrationOpen} is a ${typeof registrationOpen}.`);
