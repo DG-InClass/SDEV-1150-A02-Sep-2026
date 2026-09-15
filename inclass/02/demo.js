64ccf9139b15485666c2d5abb076cca73b91ed82
Dan Gilleland <dgilleland@nait.ca>
2026-09-15 13:04:36 -0600

    Recent prep

--- inclass/02/demo.js
+++ inclass/02/demo.js
@@ -27,4 +27,31 @@ tablesNeeded = guestCount / seatsPerTable;
 report += '\n';
 report += `\nTables needed: ${tablesNeeded}`;
 
+// TODO: Continue with Step 6....
+let snackCost = 3.75;
+let drinkCost = 1.5;
+let suppliesFee = 12;
+let snackBudget = guestCount * snackCost;
+let drinkBudget = guestCount * drinkCost;
+let totalBudget = snackBudget + drinkBudget + suppliesFee;
+
+report += '\n';
+report += `\nSnack budget: $${snackBudget}`;
+report += `\nDrink budget: $${drinkBudget}`;
+report += `\nSupplies fee: $${suppliesFee}`;
+report += `\nTotal budget: $${totalBudget}`;
+
+let signUpText = '18';
+let extraGuestText = '4';
+let signUpCount = parseInt(signUpText);
+let extraGuestCount = parseInt(extraGuestText);
+let finalGuestCount = signUpCount + extraGuestCount;
+
+report += '\n';
+report += `\nThe value ${signUpText} is a ${typeof signUpText}.`;
+report += `\nAfter parseInt(), ${signUpCount} is a ${typeof signUpCount}.`;
+report += `\nFinal guest count: ${finalGuestCount}`;
+
+// TODO: Continue with Step 9
+
 console.log(report);
