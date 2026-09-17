32becff2c01b256d1adb450fca8cbb871d4f1271
Dan Gilleland <dgilleland@nait.ca>
2026-09-17 14:11:25 -0600

    Finished lesson 3

--- inclass/03/demo.js
+++ inclass/03/demo.js
@@ -34,6 +34,11 @@ console.log();
 console.log('Objects can keep related values together');
 console.log('----------------------------------------');
 
+// An object literal syntax encloses all of the object's properties
+// inside a pair of curly braces. Inside those curly braces, we have
+// a comma-separated list of name:value pairs. The `name` portion is
+// the name of a property in the object. The `value` part is the 
+// information stored inside that property.
 let facilitator = {
     firstName: 'Avery',
     lastName: 'Chen',
@@ -61,3 +66,50 @@ console.log(workshop);
 console.log();
 
 // TODO: Next Class - Built-in Objects
+console.log('Date objects');
+console.log('------------');
+
+let today = new Date(); // an object with the current date and time
+//          \_/ \____/
+// keyword  |     | Constructor function
+let workshopDate = new Date('October 22, 2026 18:30:00');
+
+console.log(`Today is ${today.toDateString()}`);
+console.log(`The workshop starts on ${workshopDate.toDateString()}`);
+console.log(`The workshop month index is ${workshopDate.getMonth()}`);
+console.log(`The workship day of the month is ${workshopDate.getDate()}`);
+console.log();
+
+console.log('Changing object state with methods');
+console.log('----------------------------------');
+
+let reminderDate = new Date(workshopDate);
+reminderDate.setDate(reminderDate.getDate() - 7);
+//                   \____________________/
+//                           22             -7
+//                   \_______________________/
+
+workshop.startsOn = workshopDate;
+workshop.reminderOn = reminderDate;
+
+console.log(`Reminder date: ${workshop.reminderOn.toDateString()}`);
+console.log(`Workshop date: ${workshop.startsOn.toDateString()}`);
+console.log();
+
+console.log('Another built-in object');
+console.log('-----------------------');
+
+let signupUrl = new URL('https://library.example.test/workshops/photo-basics?level=beginner');
+
+console.log(`Signup link: ${signupUrl.toString()}`);
+console.log(`Protocol: ${signupUrl.protocol}`);
+console.log(`Host: ${signupUrl.host}`);
+console.log(`Path: ${signupUrl.pathname}`);
+console.log(`Level: ${signupUrl.searchParams.get('level')}`);
+console.log();
+
+console.log('Final workshop object');
+console.log('---------------------');
+console.log(workshop);
+
+export { workshop, facilitator, signupUrl }
